280798fa406d218c8068bb504f31cf6da6385ff8
iiKaRaM <71017061+iiKaRaM@users.noreply.github.com>
2020-10-01 22:05:10 +0200

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -17,7 +17,7 @@ module.exports = {
       let xd = new MessageEmbed()
       .setDescription("▶ Resumed the music for you!")
       .setColor("YELLOW")
-      .setAuthor("Music has been Resumed!", "https://raw.githubusercontent.com/iiKaRaM/Hentai-Music-DiscordBot/")
+      .setAuthor("Music has been Resumed!", "https://raw.githubusercontent.com/iiKaRaM/Hentai-Music-DiscordBot/master/assets/Music.gif")
       return message.channel.send(xd);
     }
     return sendError("There is nothing playing in this server.", message.channel);
